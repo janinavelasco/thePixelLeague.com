ed81cca0c8c9ed50a539f483efc3ae6b54ad5d84
Rob Copeland <copeland.robert@gmail.com>
2014-01-15 11:15:20 -0500

    set up tooling

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),   
+    compass: {
+      dist: {
+        files: {
+          "public/stylesheets/main.css": "sass/*.sass"
+        },
+        options: {
+          "config": "config.rb"
+        }  
+      }
+    },
+    uglify: {
+      dist: {
+        files: {
+          'public/js/main.min.js': [
+            'bower_components/jquery/jquery.min.js',
+            'bower_components/jquery.lazyload.js',
+            'js/*.js'
+          ]
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.sass',
+        tasks: ['compass'],
+        options: {
+          livereload: true
+        }
+      },
+      js: {
+        files: 'js/*.js',
+        tasks: ['uglify'],
+        options: {
+          livereload: true 
+        } 
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+}
